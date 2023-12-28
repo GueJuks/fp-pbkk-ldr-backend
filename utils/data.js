774efc13f8625713b2a43cbd4e95c1a8ecc90291
GueJuks <guejuks@gmail.com>
2023-12-29 00:05:30 +0700

    backend program

---
+++ utils/data.js
@@ -0,0 +1,21 @@
+const items = [
+    {
+        name: 'Kaos Polos Hitam',
+        price: 1,
+        category: 'kaos',
+        image: 'https://s2.bukalapak.com/img/7049333831/w-1000/kaos_polos_hitamkaos_oblongkaos_spandexkaos_priakaos_laki_la.jpg'
+    },
+    {
+        name: 'Kaos Polos Putih',
+        price: 2,
+        category: 'kaos',
+        image: 'https://s1.bukalapak.com/img/6667513511/w-1000/kaos_polos_o_neck.jpg'
+    },
+    {
+        name: 'Celana Pendek',
+        price: 3,
+        category: 'celana',
+        image: 'https://cf.shopee.co.id/file/dbfc3dacbb76b6562b449fc670cc3d80'
+    },
+]
+module.exports = items;
